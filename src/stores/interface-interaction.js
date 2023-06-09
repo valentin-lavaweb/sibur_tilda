d44ea979f8a341d65b788a400d6f09ece184d31d
SenSkysh <kukukumaks@gmail.com>
2023-06-09 21:31:55 +0300

    Update interface-interaction.js

--- src/stores/interface-interaction.js
+++ src/stores/interface-interaction.js
@@ -201,7 +201,7 @@ export const useGameStore = defineStore("interface", {
     /* ------------------------------ */
   }),
   getters: {
-    auth(){
+    auth(state){
       authClient.interceptors.response.use(
         (response) => {
           return response;
@@ -210,9 +210,9 @@ export const useGameStore = defineStore("interface", {
           if (
             error.response &&
             [401, 419].includes(error.response.status) &&
-            Boolean(this.admin)
+            Boolean(state.admin)
           ) {
-            this.logout();
+            state.logout();
           }
           return Promise.reject(error);
         }
@@ -220,7 +220,7 @@ export const useGameStore = defineStore("interface", {
 
       return auth
     },
-    api(){
+    api(state){
       apiClient.interceptors.response.use(
         (response) => {
           return response;
@@ -229,9 +229,9 @@ export const useGameStore = defineStore("interface", {
           if (
             error.response &&
             [401, 419].includes(error.response.status) &&
-            Boolean(this.admin)
+            Boolean(state.admin)
           ) {
-            this.logout();
+            state.logout();
           }
           return Promise.reject(error);
         }
