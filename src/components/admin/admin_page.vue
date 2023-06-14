f5ff4476c85568ffba908e138614dc52dce5f204
SenSkysh <kukukumaks@gmail.com>
2023-06-14 14:31:40 +0300

    Update admin_page.vue

--- src/components/admin/admin_page.vue
+++ src/components/admin/admin_page.vue
@@ -10,210 +10,182 @@ import login_comp from "@/components/admin/login.vue";
 // import gallery_table from "@/components/admin/tables/gallery.vue";
 
 export default {
-  name: "admin_page",
-  data() {
-    let interaction = useGameStore();
-    return{
-        interaction: interaction,
-        searchValue: null,
-    }
-  },
-  components:{
-    login_comp,
-    // Vue3EasyDataTable,
-    // personal_awards,
-    // command_awards,
-    // personal_award_sections,
-    // gallery_table
-  },
-  methods: {
-    checkAuth(){
-        this.interaction.getAuthAdmin().catch(e => console.warn(e)).then(e => console.debug(e));
-    }
-  },
-  mounted() {
-    let tableContainer  = this.$refs.tableContainer;
-    let pos = { start: 0, end: 0 };
+    name: "admin_page",
+    data() {
+        let interaction = useGameStore();
+        return {
+            interaction: interaction,
+            searchValue: null,
+        }
+    },
+    components: {
+        login_comp,
+        // Vue3EasyDataTable,
+        // personal_awards,
+        // command_awards,
+        // personal_award_sections,
+        // gallery_table
+    },
+    methods: {
+        checkAuth() {
+            this.interaction.getAuthAdmin().catch(e => console.warn(e)).then(e => console.debug(e));
+        }
+    },
+    mounted() {
+        let tableContainer = this.$refs.tableContainer;
+        let pos = { start: 0, current: 0, end: 0 };
 
-    tableContainer.addEventListener('pointerdown', (e)=>{
-        
-        pos = {
-            start: 0, 
-            end: 0
-        };
-        table.style.cursor = 'grabbing';
-        table.style.userSelect = 'none';
+        tableContainer.addEventListener('pointerdown', (e) => {
 
-        tableContainer.setPointerCapture(e.pointerId);
 
-        tableContainer.addEventListener('pointermove', (e)=>{
+            let table = tableContainer.querySelector('.vue3-easy-data-table__main');
+
+            if (table) {
+                pos.start = pos.current = pos.end = e.clientX;
+
+                tableContainer.style.cursor = 'grabbing';
+                tableContainer.style.userSelect = 'none';
+
+                tableContainer.setPointerCapture(e.pointerId);
+
+                const movelistener = (e) => {
+
+                    pos.end = e.clientX;
+                    let delta = pos.end - pos.current;
+                    pos.current = e.clientX;
+                    table.scrollLeft -= delta;
+                }
+
+                const uplistener = (e) => {
+                    pos.end = e.clientX;
+
+                    tableContainer.removeEventListener('pointermove', movelistener);
+                    tableContainer.removeEventListener('pointerup', uplistener);
+
+                    tableContainer.style.cursor = 'auto';
+                    tableContainer.style.userSelect = 'all';
+
+                }
+
+
+
+                tableContainer.addEventListener('pointermove', movelistener);
+
+                tableContainer.addEventListener('pointerup', uplistener);
+
+            }
+
+
+
+
+
+
 
         });
-        tableContainer.addEventListener('pointerup', (e)=>{
 
-        });
+        this.checkAuth();
 
+    },
+    computed: {
 
+    },
+    watch: {
 
-    });
-
-
-    const handleDown = function (elem) {
-        
-    };
-
-    const mouseMoveHandler = function (e) {
-        const dx = e.clientX - pos.x;
-        wrapper.scrollLeft = pos.left - dx;
-    };
-
-    const mouseUpHandler = function () {
-        wrapper.removeEventListener('mousemove', mouseMoveHandler);
-        wrapper.removeEventListener('mouseup', mouseUpHandler);
-
-        table.style.cursor = 'auto';
-        table.style.userSelect = 'all';
-    };
-
-
-
-
-
-
-
-    function scroll(){
-let speed = 2; // Скорость скролла.
-
-let scroll = document.querySelector('.scroll');
-
-let left = 0; // отпустили мышку - сохраняем положение скролла
-let drag = false;
-let coorX = 0; // нажали мышку - сохраняем координаты.
-
-scroll.addEventListener('mousedown', function(e) {
-  drag = true;
-  coorX = e.pageX - this.offsetLeft;
-});
-document.addEventListener('mouseup', function() {
-  drag = false;
-  left = scroll.scrollLeft;
-});
-scroll.addEventListener('mousemove', function(e) {
-  if (drag) {
-    this.scrollLeft = left + (e.pageX - this.offsetLeft - coorX)*speed;
-  }
-});
-
-}
-
-    
-
-
-    
-
-
-    this.checkAuth();
-
-  },
-  computed:{
-    
-  },
-  watch:{
-
-  },
+    },
 };
 </script>
 
 
 <template>
-  <div class="wrapper">
-    <Teleport to="body">
-        <transition name="openPage" mode="out-in" appear>
-            <login_comp v-if="!interaction.admin"/>
-        </transition>
-    </Teleport>
-    <div class="wrapper-block" v-show="interaction.admin">
-      <div class="content-block">
-        <nav>
-            <div class="logo" @click="checkAuth">
-                <img src="/img/logo.svg" alt="logo" />
-                <div class="logo-text">
-                    энергия<br>
-                    признания
+    <div class="wrapper">
+        <Teleport to="body">
+            <transition name="openPage" mode="out-in" appear>
+                <login_comp v-if="!interaction.admin" />
+            </transition>
+        </Teleport>
+        <div class="wrapper-block" v-show="interaction.admin">
+            <div class="content-block">
+                <nav>
+                    <div class="logo" @click="checkAuth">
+                        <img src="/img/logo.svg" alt="logo" />
+                        <div class="logo-text">
+                            энергия<br>
+                            признания
+                        </div>
+                    </div>
+                    <div class="login" @click="interaction.logout(), $router.push({ name: 'admin_page' })">
+                        Выйти
+                    </div>
+                </nav>
+                <div class="search_panel">
+                    <div class="search-img">
+                        <img src="/img/search_grey.svg" alt="search" />
+                    </div>
+                    <input type="text" placeholder="Введите запрос" v-model="searchValue">
                 </div>
-            </div>
-            <div class="login"
-                @click="interaction.logout(), $router.push({name: 'admin_page'})">
-                Выйти
-            </div>
-        </nav>
-        <div class="search_panel">
-            <div class="search-img">
-                <img src="/img/search_grey.svg" alt="search"/>
-            </div>
-            <input type="text" placeholder="Введите запрос" v-model="searchValue">
-        </div>
-        <section class="section_tabs">
-            <div class="tab" 
-                :class="{active: this.$route.name == 'command_awards_table'}"
-                @click="this.$router.push({name: 'command_awards_table'})">
-                командные награды
-            </div>
-            <div class="tab"
-                :class="{active: this.$route.name == 'personal_awards_table'}"
-                @click="this.$router.push({name: 'personal_awards_table'})">
-                персональные награды
-            </div>
-            <div class="tab"
-                :class="{active: this.$route.name == 'personal_award_sections_table'}"
-                @click="this.$router.push({name: 'personal_award_sections_table'})">
-                разделы для персональных наград
-            </div>
-            <div class="tab"
-                :class="{active: this.$route.name == 'gallery_table'}"
-                @click="this.$router.push({name: 'gallery_table'})">
-                галерея
-            </div>
-        </section>
-        <section class="content_table" ref="tableContainer">
-            <RouterView :search="searchValue"/>
-            <!-- <personal_award_sections :search="searchValue" v-if="this.$route.name == 'personal_award_sections_table'"/>
+                <section class="section_tabs">
+                    <div class="tab" :class="{ active: this.$route.name == 'command_awards_table' }"
+                        @click="this.$router.push({ name: 'command_awards_table' })">
+                        командные награды
+                    </div>
+                    <div class="tab" :class="{ active: this.$route.name == 'personal_awards_table' }"
+                        @click="this.$router.push({ name: 'personal_awards_table' })">
+                        персональные награды
+                    </div>
+                    <div class="tab" :class="{ active: this.$route.name == 'personal_award_sections_table' }"
+                        @click="this.$router.push({ name: 'personal_award_sections_table' })">
+                        разделы для персональных наград
+                    </div>
+                    <div class="tab" :class="{ active: this.$route.name == 'gallery_table' }"
+                        @click="this.$router.push({ name: 'gallery_table' })">
+                        галерея
+                    </div>
+                </section>
+                <section class="content_table" ref="tableContainer">
+                    <RouterView :search="searchValue" />
+                    <!-- <personal_award_sections :search="searchValue" v-if="this.$route.name == 'personal_award_sections_table'"/>
             <personal_awards :search="searchValue" v-if="this.$route.name == 'personal_awards_table'"/>
             <command_awards :search="searchValue" v-if="this.$route.name == 'command_awards_table'"/> -->
-        </section>
-      </div>
+                </section>
+            </div>
+        </div>
     </div>
-  </div>
-  
 </template>
 
 <style>
-.vue3-easy-data-table__body tr td:first-child{
+.vue3-easy-data-table__body tr td:first-child {
     background-color: #EDF7F8;
 }
-.vue3-easy-data-table__main.hoverable tr:hover td:first-child{
+
+.vue3-easy-data-table__main.hoverable tr:hover td:first-child {
     background-color: #EDF7F8;
 }
+
 /* .vue3-easy-data-table__main.hoverable tr:hover td:hover{
     background-color: transparent;
 } */
-.vue3-easy-data-table__body tr td{transition: all 0.25s ease;}
+.vue3-easy-data-table__body tr td {
+    transition: all 0.25s ease;
+}
 
 
 .easy-data-table__rows-selector ul.select-items.inside li,
-.buttons-pagination .item{
+.buttons-pagination .item {
     transition: all 0.25s ease;
 }
+
 .easy-data-table__rows-selector ul.select-items.inside li:hover,
-.buttons-pagination .item:hover{
+.buttons-pagination .item:hover {
     background-color: rgb(236, 236, 236);
 }
+
 .easy-data-table__rows-selector ul.select-items.inside li.selected:hover,
-.buttons-pagination .item.button.active:hover{
+.buttons-pagination .item.button.active:hover {
     background-color: var(--nipigasColorMain-hover);
 }
 
-.vue3-easy-data-table__footer, .vue3-easy-data-table__footer *{
+.vue3-easy-data-table__footer,
+.vue3-easy-data-table__footer * {
     flex-direction: row;
 }
 
@@ -227,35 +199,36 @@ scroll.addEventListener('mousemove', function(e) {
 .easy-data-table__rows-selector .rows-input__wrapper{
     flex-direction: row;
 } */
-
 </style>
 <style scoped>
-
-.content_table div{
-  display: unset;
-  justify-content: unset;
-  align-items: unset;  
-  flex-direction: unset;
-}
-.wrapper{
-  width: 100%;
-  height: 100%;
-}
-.wrapper-block{
-   width: 1800px;
-   height: fit-content;
-   margin: 0px 0 50px 0;
-   overflow-x: auto;
+.content_table div {
+    display: unset;
+    justify-content: unset;
+    align-items: unset;
+    flex-direction: unset;
 }
 
-.content-block{
-  margin: 0 0 0 0;
-  width: 100%;
-  justify-content: flex-start;
-  align-items: flex-start;
-  z-index: 2;
+.wrapper {
+    width: 100%;
+    height: 100%;
 }
-nav{
+
+.wrapper-block {
+    width: 1800px;
+    height: fit-content;
+    margin: 0px 0 50px 0;
+    overflow-x: auto;
+}
+
+.content-block {
+    margin: 0 0 0 0;
+    width: 100%;
+    justify-content: flex-start;
+    align-items: flex-start;
+    z-index: 2;
+}
+
+nav {
     position: relative;
     display: flex;
     flex-direction: row;
@@ -264,15 +237,18 @@ nav{
     width: 100%;
     height: 100px;
 }
-.logo{
+
+.logo {
     flex-direction: row;
 }
+
 .logo-text {
     margin: 0 0 0 5px;
     line-height: 87.1%;
     color: var(--nipigasColorMain);
 }
-.login{
+
+.login {
     font-size: 16px;
     font-weight: 500;
     text-decoration-line: underline;
@@ -280,7 +256,8 @@ nav{
     cursor: pointer;
     transition: all 0.25s ease;
 }
-.login:hover{
+
+.login:hover {
     color: var(--nipigasColorMain-hover);
 }
 
@@ -288,7 +265,7 @@ nav{
 
 
 
-.search_panel{
+.search_panel {
     width: 100%;
     position: relative;
     flex-direction: row;
@@ -296,10 +273,12 @@ nav{
     flex-direction: row;
     margin: 50px 0 0 0;
 }
-.search-img{
-   margin: 0 5px 0 0;
+
+.search-img {
+    margin: 0 5px 0 0;
 }
-.search_panel input{
+
+.search_panel input {
     width: 140px;
     border: none;
     border-bottom: 1px solid #D6D6D6;
@@ -307,23 +286,26 @@ nav{
     color: var(--nipigasColorMain);
     font-style: italic;
 }
-.search_panel input::placeholder{
+
+.search_panel input::placeholder {
     font-size: 14px;
     color: #D9D9D9;
     font-style: italic;
 }
 
 
-section{
+section {
     margin: 50px 0 0 0;
     position: relative;
     display: flex;
 }
-.tab.active{
+
+.tab.active {
     color: var(--white);
     background-color: var(--nipigasColorMain);
 }
-.tab{
+
+.tab {
     border-top-right-radius: 10px;
     border-top-left-radius: 10px;
     border: 1px solid var(--nipigasColorMain);
@@ -334,12 +316,13 @@ section{
     cursor: pointer;
     transition: all 0.25s ease;
 }
-.tab:hover{
+
+.tab:hover {
     background-color: var(--nipigasColorMain-hover);
     color: var(--white);
 }
 
-.content_table{
+.content_table {
     margin: 0;
     width: 100%;
     height: fit-content;
@@ -352,38 +335,32 @@ section{
 
 
 @media (max-width: 1700px) {
-    .wrapper-block{
+    .wrapper-block {
         width: 90vw;
     }
 }
-@media (max-width: 1440px) {
 
-}
-@media (max-width: 1024px) {
+@media (max-width: 1440px) {}
 
-}
-@media (max-width: 980px) {
+@media (max-width: 1024px) {}
 
-}
+@media (max-width: 980px) {}
+
 @media (max-width: 768px) {
 
-    .section_tabs{
+    .section_tabs {
         flex-wrap: wrap;
     }
-    .tab{
+
+    .tab {
         border-radius: 0px;
     }
 }
-@media (max-width: 640px) {
 
-}
-@media (max-width: 480px) {
+@media (max-width: 640px) {}
 
-}
-@media (max-width: 420px) {
+@media (max-width: 480px) {}
 
-}
-@media (max-width: 380px) {
+@media (max-width: 420px) {}
 
-}
-</style>
+@media (max-width: 380px) {}</style>
